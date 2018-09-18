04cc6102c3ff65fd9df0a12c3e156cdac70c7126
Thuvarakan Thanabalasingam <ThuvarakanThanabalas@cmail.carleton.ca>
2018-09-18 15:02:03 -0400

    init: Created Maven Project - added Junit as a Dependency to pom.xml

---
+++ junit/src/main/java/com/comp3004/junit/App.java
@@ -0,0 +1,13 @@
+package com.comp3004.junit;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
